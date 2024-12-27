cdbe66ee43a286e200de47777e5aaf8a6311945a
StLeonid <stolbovla@yandex.ru>
2024-12-27 19:33:28 +0300

    Indigo: test

--- core/indigo-core/layout/molecule_cleaner_2d.h
+++ core/indigo-core/layout/molecule_cleaner_2d.h
@@ -14,7 +14,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- ***************************************************************************/
+ ****************************************************************************/
 
 #ifndef __molecule_cleaner_2d__
 #define __molecule_cleaner_2d__
